5999811d3a31953447f3aef65cbd9988510a89b3
Роман <r.kusurgashev@gmail.com>
2021-12-27 12:27:46 +0700

    1

---
+++ Kursovay/vchmat3/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace vchmat6
+
+{
+    public partial class Form1 : Form
+    {
+        Airplane a1 = new Airplane();
+        Airplane a2 = new Airplane(100, 100);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+
+        {
+            a1.NewAirplane();
+            pictureBox2.Location = new Point(a1.x, a1.y);
+            //a2.NewAirplane();
+            pictureBox3.Location = new Point(a2.x, a2.y);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+            for (; ;)
+            {
+                Thread.Sleep(50);
+                a1.MovingAIrplane();
+                pictureBox2.Location = new Point(a1.x, a1.y);
+                if (a1.x > 500 || a1.y > 1250)
+                    break;
+            }
+        }
+         
+
+    }
+}
---
+++ Kursovay/vchmat3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vchmat6
+{
+
+    class Airplane
+    {
+        public
+       int x, y;
+        int degree;
+        public Airplane(){}
+
+        public Airplane(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+        
+    
+        public void NewAirplane()
+        {
+            y = 200;
+            x = 100;
+        }
+
+        public void MovingAIrplane( )
+        {
+            x+=3;
+            y+=3;
+        }
+
+    }
+
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
