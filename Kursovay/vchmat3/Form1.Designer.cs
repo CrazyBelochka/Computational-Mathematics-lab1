99b77356da9b517e12417025b9996da9db82ab09
Роман <r.kusurgashev@gmail.com>
2021-12-30 09:51:40 +0700

    Kursovay

--- Kursovay/vchmat3/Form1.Designer.cs
+++ Kursovay/vchmat3/Form1.Designer.cs
@@ -44,6 +44,14 @@ namespace vchmat6
             this.pictureBox9 = new System.Windows.Forms.PictureBox();
             this.pictureBox10 = new System.Windows.Forms.PictureBox();
             this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.button7 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -55,32 +63,34 @@ namespace vchmat6
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackColor = System.Drawing.Color.LightGray;
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.Location = new System.Drawing.Point(-32, -74);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1273, 661);
+            this.pictureBox1.Size = new System.Drawing.Size(1972, 1135);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1095, 135);
+            this.button1.Location = new System.Drawing.Point(1654, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 37);
+            this.button1.Size = new System.Drawing.Size(115, 56);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Начать движение";
+            this.button1.Text = "Короткое движение";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(1095, 77);
+            this.button2.Location = new System.Drawing.Point(1548, 12);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 42);
+            this.button2.Size = new System.Drawing.Size(100, 56);
             this.button2.TabIndex = 2;
             this.button2.Text = "Изобразить самолеты";
             this.button2.UseVisualStyleBackColor = true;
@@ -88,100 +98,192 @@ namespace vchmat6
             // 
             // pictureBox2
             // 
+            this.pictureBox2.BackColor = System.Drawing.Color.LightGray;
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(1128, 310);
+            this.pictureBox2.Location = new System.Drawing.Point(1034, 160);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(38, 24);
+            this.pictureBox2.Size = new System.Drawing.Size(51, 55);
             this.pictureBox2.TabIndex = 3;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
+            this.pictureBox3.BackColor = System.Drawing.Color.LightGray;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(1172, 189);
+            this.pictureBox3.Location = new System.Drawing.Point(319, 160);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(38, 24);
+            this.pictureBox3.Size = new System.Drawing.Size(52, 55);
             this.pictureBox3.TabIndex = 4;
             this.pictureBox3.TabStop = false;
             // 
             // pictureBox4
             // 
             this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(1128, 280);
+            this.pictureBox4.Location = new System.Drawing.Point(795, 160);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(38, 24);
+            this.pictureBox4.Size = new System.Drawing.Size(50, 55);
             this.pictureBox4.TabIndex = 5;
             this.pictureBox4.TabStop = false;
             // 
             // pictureBox5
             // 
+            this.pictureBox5.BackColor = System.Drawing.Color.LightGray;
             this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
-            this.pictureBox5.Location = new System.Drawing.Point(1128, 250);
+            this.pictureBox5.Location = new System.Drawing.Point(636, 160);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(38, 24);
+            this.pictureBox5.Size = new System.Drawing.Size(51, 55);
             this.pictureBox5.TabIndex = 6;
             this.pictureBox5.TabStop = false;
             // 
             // pictureBox6
             // 
+            this.pictureBox6.BackColor = System.Drawing.Color.LightGray;
             this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
-            this.pictureBox6.Location = new System.Drawing.Point(1128, 219);
+            this.pictureBox6.Location = new System.Drawing.Point(483, 160);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(38, 24);
+            this.pictureBox6.Size = new System.Drawing.Size(52, 55);
             this.pictureBox6.TabIndex = 7;
             this.pictureBox6.TabStop = false;
             // 
             // pictureBox7
             // 
+            this.pictureBox7.BackColor = System.Drawing.Color.LightGray;
             this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
-            this.pictureBox7.Location = new System.Drawing.Point(1128, 189);
+            this.pictureBox7.Location = new System.Drawing.Point(400, 160);
             this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(38, 24);
+            this.pictureBox7.Size = new System.Drawing.Size(54, 55);
             this.pictureBox7.TabIndex = 8;
             this.pictureBox7.TabStop = false;
             // 
             // pictureBox8
             // 
             this.pictureBox8.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox8.Image")));
-            this.pictureBox8.Location = new System.Drawing.Point(1172, 280);
+            this.pictureBox8.Location = new System.Drawing.Point(874, 160);
             this.pictureBox8.Name = "pictureBox8";
-            this.pictureBox8.Size = new System.Drawing.Size(38, 24);
+            this.pictureBox8.Size = new System.Drawing.Size(51, 55);
             this.pictureBox8.TabIndex = 9;
             this.pictureBox8.TabStop = false;
             // 
             // pictureBox9
             // 
             this.pictureBox9.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox9.Image")));
-            this.pictureBox9.Location = new System.Drawing.Point(1172, 250);
+            this.pictureBox9.Location = new System.Drawing.Point(713, 160);
             this.pictureBox9.Name = "pictureBox9";
-            this.pictureBox9.Size = new System.Drawing.Size(38, 24);
+            this.pictureBox9.Size = new System.Drawing.Size(51, 55);
             this.pictureBox9.TabIndex = 10;
             this.pictureBox9.TabStop = false;
             // 
             // pictureBox10
             // 
+            this.pictureBox10.BackColor = System.Drawing.Color.LightGray;
             this.pictureBox10.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox10.Image")));
-            this.pictureBox10.Location = new System.Drawing.Point(1172, 219);
+            this.pictureBox10.Location = new System.Drawing.Point(557, 160);
             this.pictureBox10.Name = "pictureBox10";
-            this.pictureBox10.Size = new System.Drawing.Size(38, 24);
+            this.pictureBox10.Size = new System.Drawing.Size(53, 55);
             this.pictureBox10.TabIndex = 11;
             this.pictureBox10.TabStop = false;
             // 
             // pictureBox11
             // 
             this.pictureBox11.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox11.Image")));
-            this.pictureBox11.Location = new System.Drawing.Point(1172, 310);
+            this.pictureBox11.Location = new System.Drawing.Point(955, 160);
             this.pictureBox11.Name = "pictureBox11";
-            this.pictureBox11.Size = new System.Drawing.Size(38, 24);
+            this.pictureBox11.Size = new System.Drawing.Size(53, 55);
             this.pictureBox11.TabIndex = 12;
             this.pictureBox11.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1775, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 56);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Иммитация поссадки";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1454, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 56);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Иммитация маневра пилотов";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1354, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(80, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(1351, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Введите число самолетов";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1354, 116);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(80, 37);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "Ввести";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1775, 74);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(101, 60);
+            this.button6.TabIndex = 18;
+            this.button6.Text = "Автоматическое движение";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox12.Image")));
+            this.pictureBox12.Location = new System.Drawing.Point(1190, 351);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(54, 50);
+            this.pictureBox12.TabIndex = 19;
+            this.pictureBox12.TabStop = false;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(1354, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(94, 56);
+            this.button7.TabIndex = 20;
+            this.button7.Text = "Иммитация взлета";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
-            this.ClientSize = new System.Drawing.Size(1234, 461);
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.pictureBox11);
             this.Controls.Add(this.pictureBox10);
             this.Controls.Add(this.pictureBox9);
@@ -208,7 +310,9 @@ namespace vchmat6
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -227,6 +331,14 @@ namespace vchmat6
         private System.Windows.Forms.PictureBox pictureBox9;
         private System.Windows.Forms.PictureBox pictureBox10;
         private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.Button button7;
     }
 }
 
--- Kursovay/vchmat3/Program.cs
+++ Kursovay/vchmat3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,30 +10,192 @@ namespace vchmat6
 
     class Airplane
     {
-        public
-       int x, y;
-        int degree;
-        public Airplane(){}
 
-        public Airplane(int _x, int _y)
+        public int x, y;
+        public double degree;
+        public int takeOff_checker = 0;
+        protected int number_planes_on_screen;
+        /*public int CheckPlanesOnScreen(Airplane[] arrayAirplanes, int amount_planes)
+        {
+            number_planes_on_screen = amount_planes-1;
+            for (int i = 1; i < amount_planes; i++)
+            {
+                if(arrayAirplanes[i].x > 1920 || arrayAirplanes[i].x < 0 || arrayAirplanes[i].y > 1080 || arrayAirplanes[i].y < 0)
+                {
+                    number_planes_on_screen--;
+                }
+            }
+            return (number_planes_on_screen);
+        }*/
+    }
+    class AirplaneInAir : Airplane
+    {
+        public void MovingAirplane()
+        {
+            double x1, y1;
+
+
+            x1 = 10 * Math.Cos(degree * 3.14 / 180);
+            y1 = 10 * Math.Sin(degree * 3.14 / 180);
+            if (x1 > 0 && y1 > 0)
+            {
+                x += (int)x1;
+                y -= (int)y1;
+            }
+            if (x1 > 0 && y1 < 0)
+            {
+                x += (int)x1;
+                y -= (int)y1;
+            }
+            if (x1 < 0 && y1 < 0)
+            {
+                x += (int)x1;
+                y -= (int)y1;
+            }
+            if (x1 < 0 && y1 > 0)
+            {
+                x += (int)x1;
+                y -= (int)y1;
+            }
+        }
+
+        public void Maneuver(Airplane[] arrayAirplanes, int amount_planes)
+        {
+            //   int checker = 0;
+            for (int j = 1; j < amount_planes; j++)
+            {
+                for (int i = 1; i < amount_planes; i++)
+                {
+                    if (i == j)
+                    {
+                        break;
+
+                    }
+
+
+                    if (Math.Abs(arrayAirplanes[j].x - arrayAirplanes[i].x) <= 100 && Math.Abs(arrayAirplanes[j].y - arrayAirplanes[i].y) <= 100)
+                    {
+
+                        if (arrayAirplanes[j].takeOff_checker == 0 && arrayAirplanes[i].takeOff_checker == 0)
+                        {
+                            if (arrayAirplanes[j].degree > 135 && arrayAirplanes[j].degree < 225)
+                            {
+                                arrayAirplanes[j].x--;
+                                arrayAirplanes[j].y--;
+                                arrayAirplanes[i].x++;
+                                arrayAirplanes[i].y++;
+                            }
+                            if (arrayAirplanes[j].degree >= 225 && arrayAirplanes[j].degree < 315)
+                            {
+                                arrayAirplanes[j].x++;
+                                arrayAirplanes[j].y--;
+                                arrayAirplanes[i].x--;
+                                arrayAirplanes[i].y++;
+
+                            }
+                            if (arrayAirplanes[j].degree >= 315 || arrayAirplanes[j].degree <= 45)
+                            {
+                                arrayAirplanes[j].x++;
+                                arrayAirplanes[j].y++;
+                                arrayAirplanes[i].x--;
+                                arrayAirplanes[i].y--;
+
+                            }
+                            if (arrayAirplanes[j].degree > 45 && arrayAirplanes[j].degree <= 135)
+                            {
+                                arrayAirplanes[j].x--;
+                                arrayAirplanes[j].y++;
+                                arrayAirplanes[i].x++;
+                                arrayAirplanes[i].y--;
+
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+    class AiplaneOnEarth : AirplaneInAir
+    {
+        public AiplaneOnEarth()
+        {
+            Random rndy = new Random();
+            y = rndy.Next(50, 900);
+            Random rndx = new Random();
+            x = rndy.Next(50, 1800);
+            Random rnddegree = new Random();
+            degree = rnddegree.Next(0, 360);
+        }
+
+
+        public AiplaneOnEarth(int _x, int _y, int _degree)
         {
             x = _x;
             y = _y;
-        }
-        
-    
-        public void NewAirplane()
-        {
-            y = 200;
-            x = 100;
+            degree = _degree;
         }
 
-        public void MovingAIrplane( )
+        public void Landing(Airplane[] arrayAirplanes, int i, int amount_planes)
         {
-            x+=3;
-            y+=3;
-        }
+            bool checker2 = false;
+            for (int z = 1; z < amount_planes; z++)
+            {
+                if (arrayAirplanes[z].takeOff_checker == 1)
+                {
+                    checker2 = true;
+                }
+            }
+            if ((Math.Abs(arrayAirplanes[i].x - 150) <= 100) && (Math.Abs(arrayAirplanes[i].y - 500) <= 100) && checker2 == false)
+            {
+                if (arrayAirplanes[i].x > 150)
+                {
+                    arrayAirplanes[i].x--;
+                }
+                if (arrayAirplanes[i].x < 150)
+                {
+                    arrayAirplanes[i].x++;
+                }
+                if (arrayAirplanes[i].y > 500)
+                {
+                    arrayAirplanes[i].y--;
+                }
+                if (arrayAirplanes[i].y < 500)
+                {
+                    arrayAirplanes[i].y++;
+                }
+                if (arrayAirplanes[i].x == 150 && arrayAirplanes[i].y < 500)
+                {
+                    arrayAirplanes[i].y++;
+                }
+                if (arrayAirplanes[i].x == 150 && arrayAirplanes[i].y > 500)
+                {
+                    arrayAirplanes[i].y--;
+                }
+                if (arrayAirplanes[i].x > 150 && arrayAirplanes[i].y == 500)
+                {
+                    arrayAirplanes[i].x--;
+                }
+                if (arrayAirplanes[i].x < 150 && arrayAirplanes[i].y == 500)
+                {
+                    arrayAirplanes[i].y++;
+                }
+                if (arrayAirplanes[i].x == 150 && arrayAirplanes[i].y == 500)
+                {
+                    arrayAirplanes[i].takeOff_checker = 1;
+                }
 
+            }
+        }
+        public void Landing_in_Airport(Airplane[] arrayAirplanes, int num)
+        {
+            arrayAirplanes[num].x+=10;
+        }
+        public void TakeOff_preparing(Airplane[] arrayAirplanes, int num)
+        {      
+            arrayAirplanes[num].x += 10;
+        }
     }
 
     static class Program
@@ -48,4 +211,6 @@ namespace vchmat6
             Application.Run(new Form1());
         }
     }
+    
+
 }
